48288a47acf48c412d893d685e2d09fc2e8e1806
Isabella Bispo <isabellabispobispo@gmail.com>
2026-02-25 19:01:22 -0300

    estudos de condições

---
+++ aula11/ex008.js
@@ -0,0 +1,9 @@
+var vel = 78
+
+console.log(`A velocidade do seu carro é ${vel}KM/H`)
+
+if (vel > 60) {
+    console.log('voce ultrapassou a velocidae permitida. MULTADO')
+}
+
+console.log('Dirija sempre usando cinto de segurança!')
